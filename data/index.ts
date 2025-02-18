faac9366bffe468a90769f7a243c5f54a4c6f9d4
Mahima Devi <72203327+mahimadevi@users.noreply.github.com>
2025-02-18 11:48:38 +0530

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -5,16 +5,16 @@ export const navItems = [
 ];
 
 export const gridItems = [
-  {
-    id: 1,
-    title: "I prioritize client collaboration, fostering open communication ",
-    description: "",
-    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
-    imgClassName: "w-full h-full",
-    titleClassName: "justify-end",
-    img: "/b1.svg",
-    spareImg: "",
-  },
+  // {
+  //   id: 1,
+  //   title: "I prioritize client collaboration, fostering open communication ",
+  //   description: "",
+  //   className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+  //   imgClassName: "w-full h-full",
+  //   titleClassName: "justify-end",
+  //   img: "/b1.svg",
+  //   spareImg: "",
+  // },
   {
     id: 2,
     title: "I'm very flexible with time zone communications",
